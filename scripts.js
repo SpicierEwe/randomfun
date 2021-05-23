2a42ed5ca7452bb1d3476237e6b3279debcc8984
SpicierEwe <83544142+SpicierEwe@users.noreply.github.com>
2021-05-23 23:28:17 +0530

    s

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ m.addEventListener(`click`, function () {
       backgroundColor: `#` + Math.trunc(Math.random() * 16777215).toString(16),
       borderRadius: Math.trunc(Math.random() * 400) + `px`,
       height: `210px`,
-      boxShadow: `5px -7px 7px 10px rgba(0,0,0,0.5)`,
+      boxShadow: `5px -7px 7px 3px rgba(0,0,0,0.5)`,
     },
     5000
   );
