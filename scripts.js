29057ae56d4284370d5160204f85dfdbbcaaaaf2
SpicierEwe <83544142+SpicierEwe@users.noreply.github.com>
2021-05-23 22:40:12 +0530

    s

--- scripts.js
+++ scripts.js
@@ -8,10 +8,10 @@ const circle = document.querySelector(`.shapes3`);
 circle.addEventListener(`click`, function () {
   circle.animate(
     {
-      width: `5000px`,
+      width: `50000px`,
       backgroundColor: `#` + Math.trunc(Math.random() * 16777215).toString(16),
       borderRadius: Math.trunc(Math.random() * 200) + `px`,
-      height: `10000px`,
+      height: `100000px`,
     },
     5000
   );
