b02e85b4d1a05f7097ea70899b03e46c00f4e9a0
SpicierEwe <83544142+SpicierEwe@users.noreply.github.com>
2021-05-23 22:55:34 +0530

    k

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ m.addEventListener(`click`, function () {
     {
       width: `400px`,
       backgroundColor: `#` + Math.trunc(Math.random() * 16777215).toString(16),
-      borderRadius: Math.trunc(Math.random() * 200) + `px`,
+      borderRadius: Math.trunc(Math.random() * 400) + `px`,
       height: `100vh`,
     },
     120000
